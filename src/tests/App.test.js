ed9202c513cfb8a9e5e1bc005a0756e3f7c831cd
Wendryo S <wendryo.sales@gmail.com>
2022-03-28 23:27:01 -0300

    Req-01 done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+  it('O primeiro link deve possuir o texto Home', () => {
+    renderWithRouter(<App />);
+    const link1 = screen.getByRole('link', { name: /Home/i });
+    expect(link1).toBeInTheDocument();
+  });
+  it('O segundo link deve possuir o texto About.', () => {
+    renderWithRouter(<App />);
+    const link2 = screen.getByRole('link', { name: /About/i });
+    expect(link2).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const link3 = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(link3).toBeInTheDocument();
+  });
+});
